d8daae58bd0f63c4e7d77a31e69e34c6cb435f68
n0lvie <147314607+n0lvie@users.noreply.github.com>
2024-12-31 00:11:35 +0500

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,7 @@
+from secrets import choice as sc;o="?";x=[0,1]
+while o:
+    if input(o+'\n')==o:
+        if (v:=sc(x))==1:o+=o[-1];x.append(0)
+        else:x.remove(0);o=o[:-1]
+    else:o=o[:-1]
+exit
