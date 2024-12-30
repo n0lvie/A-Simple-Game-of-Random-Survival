362444a5348923cead7dc6566ba729f2ec7ba115
n0lvie <147314607+n0lvie@users.noreply.github.com>
2024-12-31 00:17:01 +0500

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,8 +1,8 @@
 from secrets import choice as sc
-o="?"
-x=[0,1]
+o = "?"
+x = [0,1]
 while o:
-    if input(o+'\n') == o:
+    if input(o + '\n') == o:
         if (v := sc(x)) == 1:
             o += o[-1]
             x.append(0)
